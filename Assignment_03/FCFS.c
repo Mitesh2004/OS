5b257363a6f7a5ccbc60758537f439adc8c5ec81
Mitesh <122144445+Mitesh2004@users.noreply.github.com>
2024-09-02 20:51:15 +0530

    Update FCFS.c

--- Assignment_03/FCFS.c
+++ Assignment_03/FCFS.c
@@ -1,4 +1,101 @@
 #include <stdio.h>
+
+// Function to swap two integers
+void swap(int *a, int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+// Function to sort processes by arrival time
+void sort(int at[], int bt[], int n){
+    for (int i = 0; i < n - 1; i++){
+        for (int j = i + 1; j < n; j++){
+            if (at[i] > at[j]){
+                // Swap arrival times
+                swap(&at[i], &at[j]);
+                // Swap burst times to maintain the relation
+                swap(&bt[i], &bt[j]);
+            }
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    printf("Enter the number of processes: ");
+    scanf("%d", &n);
+
+    int at[n], bt[n], ct[n], wt[n], tat[n];
+    float wtavg = 0, tatavg = 0;
+
+    // Input arrival time and burst time
+    printf("Enter the arrival time and burst time for each process:\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("Process %d:\nArrival Time: ", i + 1);
+        scanf("%d", &at[i]);
+        printf("Burst Time: ");
+        scanf("%d", &bt[i]);
+        printf("----------------\n");
+    }
+
+    // Sort processes by arrival time
+    sort(at, bt, n);
+
+    // Calculate completion time, turnaround time, and waiting time
+    for (int i = 0; i < n; i++)
+    {
+        ct[i] = (i == 0) ? at[i] + bt[i] : (at[i] > ct[i - 1]) ? at[i] + bt[i] : ct[i - 1] + bt[i];
+        tat[i] = ct[i] - at[i];
+        wt[i] = tat[i] - bt[i];
+        wtavg += wt[i];
+        tatavg += tat[i];
+    }
+
+    // Calculate averages
+    wtavg /= n;
+    tatavg /= n;
+
+    // Print results
+    printf("\nProcess\tArrival Time\tBurst Time\tCompletion Time\tTurnaround Time\tWaiting Time\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d\t%d\t\t%d\t\t%d\t\t%d\t\t%d\n", i + 1, at[i], bt[i], ct[i], tat[i], wt[i]);
+    }
+    printf("\nAverage Waiting Time: %.2f", wtavg);
+    printf("\nAverage Turnaround Time: %.2f", tatavg);
+
+    // Print Gantt chart
+    printf("\nGantt Chart:\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("|\tP%d\t", i + 1);
+    }
+    printf("|\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d\t\t", i == 0 ? at[0] : ct[i - 1]);
+    }
+    printf("%d\n", ct[n - 1]);
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+#include <stdio.h>
 // Function to swap two processes
 void swap(int *a, int *b){
     int temp = *a;
@@ -79,3 +176,11 @@ int main(){
     printf("%d\n", ct[n - 1]);
     return 0;
 }
+*/
+
+
+
+
+
+
+
